581af7d42e93f03420b7c3132717754161f75959
Médi-Rémi Hashim <mediremi@gmail.com>
2020-05-03 22:54:24 +0100

    Finish implementing NEAT-related code

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -6,19 +6,34 @@ use crate::utils::{Screen, Tile, SCREEN_SIZE};
 
 use rand::distributions::{Distribution, Uniform};
 use rand::seq::SliceRandom;
+use rand::Rng;
 
 use std::cmp::Reverse;
+use std::collections::{HashMap, HashSet};
 use std::time::Instant;
 
+const INPUT_NODES: usize = SCREEN_SIZE * SCREEN_SIZE;
+const OUTPUT_NODES: usize = 2;
+
 const DESIRED_POPULATION: i64 = 300;
+
 // Maximum number of species in pool before weaker species are removed
 const MAX_SPECIES: usize = 30;
+
 // Maximum number of generations that a species can exist for without
 // improving its performance
 const MAX_SPECIES_STALENESS: u64 = 15;
+
 // Threshold below which a compatibility distance implies that two individuals
 // are of the same species
 const COMPATIBILITY_THRESHOLD: f64 = 3.0;
+// Coefficients used when calculating compatibility distance of two individuals.
+// While the NEAT paper distinguishes between 'excess' and 'disjoint' genes,
+// here we use the term 'disjoint' to refer to both
+const DISJOINT_COEFFICIENT: f64 = 0.4;
+const WEIGHTS_COEFFICIENT: f64 = 0.1;
+
+const MUTATION_PROBABILITY: f64 = 0.2;
 
 impl Tile {
     fn as_nn_input(self) -> f64 {
@@ -139,14 +154,27 @@ impl IndividualState {
     }
 }
 
+#[derive(Copy, Clone, PartialEq)]
 enum NodeType {
     Input,
     Hidden,
     Output,
 }
 
+#[derive(Copy, Clone)]
 struct Node(NodeType);
 
+impl Node {
+    fn is_output_node(&self) -> bool {
+        self.0 == NodeType::Output
+    }
+
+    fn is_hidden_node(&self) -> bool {
+        self.0 == NodeType::Hidden
+    }
+}
+
+#[derive(Copy, Clone)]
 struct Gene {
     in_node: usize,
     out_node: usize,
@@ -155,6 +183,7 @@ struct Gene {
     innovation_number: u64,
 }
 
+#[derive(Default)]
 struct Individual {
     nodes: Vec<Node>,
     genes: Vec<Gene>,
@@ -163,29 +192,28 @@ struct Individual {
 
 impl Individual {
     pub fn new(max_innovation_number: u64) -> Self {
-        const INPUT_NODES: usize = SCREEN_SIZE * SCREEN_SIZE;
         let mut nodes = vec![];
         for _ in 0..INPUT_NODES {
             nodes.push(Node(NodeType::Input));
         }
-        for _ in 0..2 {
+        for _ in 0..OUTPUT_NODES {
             nodes.push(Node(NodeType::Output));
         }
 
         let mut rng = rand::thread_rng();
         let input_distribution = Uniform::from(0..INPUT_NODES);
-        let output_distribtion = Uniform::from(INPUT_NODES..(INPUT_NODES + 2));
+        let output_distribution = Uniform::from(INPUT_NODES..(INPUT_NODES + OUTPUT_NODES));
         let genes = vec![
             Gene {
                 in_node: input_distribution.sample(&mut rng),
-                out_node: output_distribtion.sample(&mut rng),
+                out_node: output_distribution.sample(&mut rng),
                 innovation_number: max_innovation_number + 1,
                 weight: 1.0,
                 enabled: true,
             },
             Gene {
                 in_node: input_distribution.sample(&mut rng),
-                out_node: output_distribtion.sample(&mut rng),
+                out_node: output_distribution.sample(&mut rng),
                 innovation_number: max_innovation_number + 2,
                 weight: 1.0,
                 enabled: true,
@@ -195,7 +223,7 @@ impl Individual {
         Self {
             nodes,
             genes,
-            fitness: 0,
+            ..Self::default()
         }
     }
 
@@ -203,8 +231,32 @@ impl Individual {
         1.0 / (1.0 + (-x).exp())
     }
 
-    pub fn evaluate(&self, input: [f64; SCREEN_SIZE * SCREEN_SIZE]) -> (f64, f64) {
-        unimplemented!()
+    pub fn enabled_genes_mut(&mut self) -> Vec<&mut Gene> {
+        self.genes.iter_mut().filter(|g| g.enabled).collect()
+    }
+
+    pub fn evaluate(&self, input: [f64; INPUT_NODES]) -> (f64, f64) {
+        let mut nodes = vec![0.0; self.nodes.len()];
+        for i in 0..INPUT_NODES {
+            nodes[i] = input[i];
+        }
+        let mut incoming = HashMap::new();
+        for gene in &self.genes {
+            if !gene.enabled {
+                continue;
+            }
+            let entry = incoming.entry(gene.out_node).or_insert(vec![]);
+            entry.push((gene.in_node, gene.weight));
+        }
+        for i in (INPUT_NODES + OUTPUT_NODES)..self.nodes.len() {
+            if let Some(incoming) = incoming.get(&i) {
+                let sum = incoming
+                    .iter()
+                    .fold(0.0, |acc, (node, weight)| acc + (nodes[*node] * weight));
+                nodes[i] = Self::sigmoid(sum);
+            }
+        }
+        (nodes[INPUT_NODES], nodes[INPUT_NODES + 1])
     }
 }
 
@@ -217,7 +269,7 @@ struct Species {
 }
 
 impl Species {
-    fn size(&self) -> usize {
+    fn len(&self) -> usize {
         self.members.len()
     }
 
@@ -265,21 +317,169 @@ impl Ai {
         }
     }
 
-    fn adjusted_fitness(fitness: u64, species_size: usize) -> u64 {
-        let species_size = if species_size < 20 {
-            1
-        } else {
-            species_size as u64
-        };
+    fn adjusted_fitness(fitness: u64, species_size: u64) -> u64 {
         fitness / species_size
     }
 
     fn cross_over(a: &Individual, b: &Individual) -> Individual {
-        unimplemented!()
+        // Ensure A is fitter than B
+        let (a, b) = if a.fitness < b.fitness {
+            (b, a)
+        } else {
+            (a, b)
+        };
+        let a_innos: HashSet<_> = a.genes.iter().map(|g| g.innovation_number).collect();
+        let b_innos: HashSet<_> = b.genes.iter().map(|g| g.innovation_number).collect();
+        let mut matching_genes: HashMap<_, _> = a_innos
+            .union(&b_innos)
+            .cloned()
+            .map(|i| (i, (None, None)))
+            .collect();
+        let mut disjoint_genes: HashMap<_, _> = a_innos
+            .difference(&b_innos)
+            .cloned()
+            .map(|i| (i, None))
+            .collect();
+        for gene in &a.genes {
+            matching_genes
+                .entry(gene.innovation_number)
+                .and_modify(|(g, _)| *g = Some(gene));
+            disjoint_genes
+                .entry(gene.innovation_number)
+                .and_modify(|g| *g = Some(gene));
+        }
+        for gene in &b.genes {
+            matching_genes
+                .entry(gene.innovation_number)
+                .and_modify(|(_, g)| *g = Some(gene));
+        }
+        let mut genes: Vec<Gene> = matching_genes
+            .values()
+            .map(|(g_a, g_b)| {
+                if rand::random() {
+                    (*g_a.unwrap()).clone()
+                } else {
+                    (*g_b.unwrap()).clone()
+                }
+            })
+            .collect();
+        let mut disjoint_genes = disjoint_genes
+            .values()
+            .map(|g| (*g.unwrap()).clone())
+            .collect();
+        genes.append(&mut disjoint_genes);
+        Individual {
+            nodes: a.nodes.clone(),
+            genes,
+            ..Individual::default()
+        }
+    }
+
+    fn mutate_add_connection(individual: &mut Individual, max_innovation_number: u64) -> u64 {
+        let mut rng = rand::thread_rng();
+        let in_distribution = Uniform::from(0..individual.nodes.len());
+        let mut in_node = in_distribution.sample(&mut rng);
+        while individual.nodes[in_node].is_output_node() || in_node == individual.nodes.len() - 1 {
+            in_node = in_distribution.sample(&mut rng);
+        }
+        let out_distribution = Uniform::from(INPUT_NODES..individual.nodes.len());
+        let mut out_node = out_distribution.sample(&mut rng);
+        // To prevent cycles, ensure out node comes after in node if out node is
+        // hidden node (output nodes come before hidden nodes, so only perform
+        // check for hidden nodes)
+        while individual.nodes[out_node].is_hidden_node() && out_node < in_node {
+            out_node = out_distribution.sample(&mut rng);
+        }
+        let innovation_number = max_innovation_number + 1;
+        individual.genes.push(Gene {
+            in_node,
+            out_node,
+            weight: 1.0,
+            enabled: true,
+            innovation_number,
+        });
+        innovation_number
+    }
+
+    fn mutate_add_node(individual: &mut Individual, max_innovation_number: u64) -> u64 {
+        let gene = {
+            let mut rng = rand::thread_rng();
+            let mut enabled_genes = individual.enabled_genes_mut();
+            let gene: &mut Gene = enabled_genes.choose_mut(&mut rng).unwrap();
+            gene.enabled = false;
+            gene.clone()
+        };
+
+        let new_node = Node(NodeType::Hidden);
+        individual.nodes.push(new_node);
+        let new_node_index = individual.nodes.len() - 1;
+
+        individual.genes.push(Gene {
+            in_node: gene.in_node,
+            out_node: new_node_index,
+            weight: 1.0,
+            enabled: true,
+            innovation_number: max_innovation_number + 1,
+        });
+        individual.genes.push(Gene {
+            in_node: new_node_index,
+            out_node: gene.out_node,
+            weight: 1.0,
+            enabled: true,
+            innovation_number: max_innovation_number + 2,
+        });
+
+        max_innovation_number + 2
+    }
+
+    fn mutate_change_weight(individual: &mut Individual, max_innovation_number: u64) -> u64 {
+        let mut rng = rand::thread_rng();
+        let mut enabled_genes = individual.enabled_genes_mut();
+        let gene: &mut Gene = enabled_genes.choose_mut(&mut rng).unwrap();
+        gene.weight = rng.gen_range(-2.0, 2.0);
+        max_innovation_number
+    }
+
+    fn mutate(mut individual: &mut Individual, max_innovation_number: u64) -> u64 {
+        let mut rng = rand::thread_rng();
+        let f = match rng.gen_range(0, 3) {
+            0 => Self::mutate_add_connection,
+            1 => Self::mutate_add_node,
+            _ => Self::mutate_change_weight,
+        };
+        f(&mut individual, max_innovation_number)
     }
 
     fn compatibility_distance(a: &Individual, b: &Individual) -> f64 {
-        unimplemented!()
+        let n = a.genes.len().max(b.genes.len());
+        let n = if n < 20 { 1.0 } else { n as f64 };
+        let a_innos: HashSet<_> = a.genes.iter().map(|g| g.innovation_number).collect();
+        let b_innos: HashSet<_> = b.genes.iter().map(|g| g.innovation_number).collect();
+        let disjoint_size = a_innos
+            .symmetric_difference(&b_innos)
+            .collect::<HashSet<_>>()
+            .len() as f64;
+        let mut weights: HashMap<_, _> = a_innos
+            .union(&b_innos)
+            .cloned()
+            .map(|i| (i, (0.0, 0.0)))
+            .collect();
+        for gene in &a.genes {
+            weights
+                .entry(gene.innovation_number)
+                .and_modify(|(w, _)| *w = gene.weight);
+        }
+        for gene in &b.genes {
+            weights
+                .entry(gene.innovation_number)
+                .and_modify(|(_, w)| *w = gene.weight);
+        }
+        let sum_of_differences = weights.values().fold(0.0, |sum, (weight_a, weight_b)| {
+            sum + (weight_a - weight_b).abs()
+        });
+        let average_weights_difference = sum_of_differences / weights.len() as f64;
+        (DISJOINT_COEFFICIENT * disjoint_size / n)
+            + (WEIGHTS_COEFFICIENT * average_weights_difference)
     }
 
     fn is_same_species(a: &Individual, b: &Individual) -> bool {
@@ -336,20 +536,17 @@ impl Ai {
     }
 
     fn population(&self) -> i64 {
-        self.pool
-            .iter()
-            .map(|species| species.size())
-            .sum::<usize>() as i64
+        self.pool.iter().map(|species| species.len()).sum::<usize>() as i64
     }
 
     // Breed individuals of same species
     fn cross_over_within_species(&mut self) {
         let mut rng = rand::thread_rng();
         for species in &mut self.pool {
-            if species.size() < 2 {
+            if species.len() < 2 {
                 continue;
             }
-            for _ in 0..species.size() {
+            for _ in 0..species.len() {
                 let parents: Vec<&Individual> =
                     species.members.choose_multiple(&mut rng, 2).collect();
                 let child = Self::cross_over(parents[0], parents[1]);
@@ -392,13 +589,24 @@ impl Ai {
         }
     }
 
-    fn mutate(&self, individual: &mut Individual) {
-        unimplemented!()
-    }
-
-    fn mutate_random_invididuals(&self) {
-        // TODO: Keep track of innovation numbers
-        unimplemented!()
+    fn mutate_random_invididuals(&mut self) {
+        let mut max_innovation_number = self
+            .pool
+            .iter()
+            .map(|species| &species.members)
+            .flatten()
+            .map(|individual| &individual.genes)
+            .flatten()
+            .map(|gene| gene.innovation_number)
+            .max()
+            .unwrap();
+        for species in &mut self.pool {
+            for mut individual in &mut species.members {
+                if rand::random::<f64>() < MUTATION_PROBABILITY {
+                    max_innovation_number = Self::mutate(&mut individual, max_innovation_number);
+                }
+            }
+        }
     }
 
     fn next_generation(&mut self) {
@@ -415,16 +623,22 @@ impl Ai {
 
     pub fn next_individual(&mut self) {
         let (species_index, individual_index) = self.current_individual;
-        let species_size = self.pool[species_index].size();
+        let species_size = self.pool[species_index].len();
         let individual = &mut self.pool[species_index].members[individual_index];
         individual.fitness =
-            Self::adjusted_fitness(self.current_individual_state.fitness(), species_size);
+            Self::adjusted_fitness(self.current_individual_state.fitness(), species_size as u64);
 
         let pool_size = self.pool.len();
 
         self.current_individual = if individual_index == species_size - 1 {
             if species_index == pool_size - 1 {
                 self.next_generation();
+                println!(
+                    "New generation (g = {}). Population = {}. Species = {}",
+                    self.generation,
+                    self.population(),
+                    self.pool.len()
+                );
                 (0, 0)
             } else {
                 (species_index + 1, 0)
